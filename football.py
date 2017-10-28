60c84b731821449fed05f3dbec37acd5c3a5f7fd
Aniket Singh <aksingh143n@gmail.com>
2017-10-28 18:33:46 +0530

    jytddfgcgn

--- football.py
+++ football.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, url_for, redirect, request
 from crudClass import Team
+import sqlite3
+
 
 app = Flask(__name__)
 
@@ -9,6 +11,47 @@ def hello_world():
     return "working Success"
 
 
+@app.route("/editTeam/<string:teamName>/", methods=["GET", "POST"])
+def editTeam(teamName):
+    conn = sqlite3.connect("../PycharmProjects/football/football.db")
+    if request.method == "GET":
+        cursor = conn.execute("SELECT  * FROM TEAM WHERE TEAM_NAME=?" , (teamName , )).fetchone()
+        b = ["teamName", "teamLogo", "country",
+             "squadPic",
+             "founded",
+             "homeGround",
+             "teamCost",
+             "teamOwner",
+             "teamSponsor",
+             "teamCoach",
+             "teamWebsite",
+             "about"]
+        teamInfo = dict(zip(b, cursor))
+        print(teamInfo)
+        conn.close()
+        return render_template("teamEditForm.html",teamInfo=teamInfo)
+
+    elif request.method == "POST":
+        t = Team(teamName)
+        about = request.form.get("teamAbout", None)
+        if about == "" or about == " ":
+            about = "This team is prominent team in league"
+        t.insert_team(teamLogo=request.form.get("teamLogo", None),
+                      squadpic=request.form.get("squadPic", None),
+                      founded=request.form.get("founded", None),
+                      homeground=request.form.get("homeGround", None),
+                      teamcost=eval(request.form.get("teamCost", 0)),
+                      teamWebsite=request.form.get("teamWebsite", None),
+                      teamowner=request.form.get("teamOwner", None),
+                      teamcoach=request.form.get("teamCoach", None),
+                      sponser=request.form.get("teamSponsor", None),
+                      country=request.form.get("country", None),
+                      about=about,
+                      operation="update")
+        print(request.form)
+        return redirect(url_for("showTeam"))
+
+
 @app.route("/addTeam/", methods=["POST", "GET"])
 def addTeam():
     if request.method == 'POST':
@@ -16,7 +59,7 @@ def addTeam():
         about = request.form.get("teamAbout", None)
         if about == "" or about == " ":
             about = "This team is prominent team in league"
-        t = Team(request.form.get("teamName",None))
+        t = Team(request.form.get("teamName", None))
         t.insert_team(teamLogo=request.form.get("teamLogo", None),
                       squadpic=request.form.get("squadPic", None),
                       founded=request.form.get("founded", None),
@@ -35,4 +78,4 @@ def addTeam():
 
 
 if __name__ == '__main__':
-    app.run(debug=True ,host="127.0.0.1", port=5000)
+    app.run(debug=True, host="127.0.0.1", port=5000)
--- hello.py
+++ hello.py
@@ -50,29 +50,6 @@ def showTeam():
     return render_template("allTeam.html", teamInformation=a)
 
 
-@app.route("/addTeam/", methods=["POST", "GET"])
-def addTeam():
-    if request.method == 'POST':
-        t = Team(request.form["teamName"])
-        about = request.form.get("teamAbout", None)
-        if about == "" or about == " ":
-            about = "This team is prominent team in league"
-
-        t.insert_team(teamLogo=request.form.get("teamLogo", None),
-                      squadpic=request.form.get("squadPic", None),
-                      founded=request.form.get("founded", None),
-                      homeground=request.form.get("homeGround", None),
-                      teamcost=eval(request.form.get("teamCost", 0)),
-                      teamWebsite=request.form.get("teamWebsite", None),
-                      teamowner=request.form.get("teamOwner", None),
-                      teamcoach=request.form.get("teamCoach", None),
-                      sponser=request.form.get("teamSponsor", None),
-                      country=request.form.get("country", None),
-                      about=about,
-                      operation="insert")
-        return redirect(url_for("showTeam"))
-    else:
-        return render_template("teamAddForm.html")
 
 
 @app.route("/editTeam/<string:teamName>/", methods=["GET", "POST"])
