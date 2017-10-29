26ebe9f081aca26a67686ef6b49d0f3b821c1cdc
Amidezcod <amanpandey5319@gmail.com>
2017-10-29 18:21:36 +0530

    da

--- populateData.py
+++ populateData.py
@@ -1,12 +1,4 @@
 import sqlite3
 
 conn = sqlite3.connect("football.db")
-cursor = conn.execute("SELECT  TEAM_NAME FROM TEAM ")
-b = ["teamName"]
-list1=[]
-for line in cursor:
-     list1.append(dict(zip(b,line)))
-
-print(list1)
-
 conn.close()
